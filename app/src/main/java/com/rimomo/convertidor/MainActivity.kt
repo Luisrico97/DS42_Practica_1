6d379b2da7d915ae96b36e58b3a25c6e66176379
Luisrico97 <luis.1997.ricoj@gmail.com>
2023-10-18 13:00:31 -0700

    Avances del diseño

--- app/src/main/java/com/rimomo/convertidor/MainActivity.kt
+++ app/src/main/java/com/rimomo/convertidor/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rimomo.convertidor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import com.rimomo.convertidor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val convertir = findViewById<Button>(R.id.button)
+        val limpiar = findViewById<Button>(R.id.button2)
+        val valor = findViewById<EditText>(R.id.editTextText)
+        val despliegue1 = findViewById<Spinner>(R.id.spinner)
+        val despliegue2 = findViewById<Spinner>(R.id.spinner2)
+        val tiposdeTemperaturas = arrayOf("°C","°F","°K")
+
+
+        despliegue1.adapter = ArrayAdapter (this, android.R.layout.simple_list_item_1, tiposdeTemperaturas)
+
+        despliegue2.adapter = ArrayAdapter (this, android.R.layout.simple_list_item_1, tiposdeTemperaturas)
     }
 }
